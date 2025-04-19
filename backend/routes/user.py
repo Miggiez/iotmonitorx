e0666bc62a3594663265d8c426c83dae4360efa6
Juan Miguel Z. Marqueses <juanmigzmarq@gmail.com>
2025-04-19 22:26:27 +0700

    Modified (User): modified it so that not users will not be able to do admin previlages

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -1,18 +1,10 @@
-from datetime import datetime
-
 from bson import ObjectId
-from configurations import influx_connection, logs_col, user_col
+from configurations import logs_col, user_col
 from fastapi import APIRouter, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from models.UserModel import User
 from passlib.context import CryptContext
 from schemas.LogSchema import log_list_serial
-from schemas.UserSchema import (
-    delete_projects_array,
-    get_project_device,
-    user_individual_serial,
-    user_list_serial,
-)
+from schemas.UserSchema import get_project_device, user_individual_serial
 
 user_router = APIRouter(prefix="/user", tags=["user"])
 SECRET_KEY = "your_super_secret_key_here"
@@ -21,35 +13,35 @@ bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 
-@user_router.post("/create/user", status_code=status.HTTP_201_CREATED)
-async def register_user(user: User):
-    # Check if the username already exists
-    if user_col.find_one({"username": user.username}):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"User with username {user.username} already exist, please use another username. Failed to create User!",
-        )
+# @user_router.post("/create/user", status_code=status.HTTP_201_CREATED)
+# async def register_user(user: User):
+#     # Check if the username already exists
+#     if user_col.find_one({"username": user.username}):
+#         raise HTTPException(
+#             status_code=status.HTTP_400_BAD_REQUEST,
+#             detail=f"User with username {user.username} already exist, please use another username. Failed to create User!",
+#         )
 
-    # Hash the password
-    hashed_password = bcrypt_context.hash(user.password)
-    db_user = User(
-        username=user.username,
-        email=user.email,
-        password=hashed_password,
-        project=[],
-        logs=[],
-        role=user.role,
-        created_at=datetime.now(),
-        updated_at=datetime.now(),
-    )  # Added email field
-    # Store the user in the database
-    user_col.insert_one(dict(db_user))
-    return {"message": "User created successfully", "user": db_user.username}
+#     # Hash the password
+#     hashed_password = bcrypt_context.hash(user.password)
+#     db_user = User(
+#         username=user.username,
+#         email=user.email,
+#         password=hashed_password,
+#         project=[],
+#         logs=[],
+#         role=user.role,
+#         created_at=datetime.now(),
+#         updated_at=datetime.now(),
+#     )  # Added email field
+#     # Store the user in the database
+#     user_col.insert_one(dict(db_user))
+#     return {"message": "User created successfully", "user": db_user.username}
 
 
-@user_router.get("/get/users", status_code=status.HTTP_200_OK)
-async def find_all_users():
-    return user_list_serial(user_col.find())
+# @user_router.get("/get/users", status_code=status.HTTP_200_OK)
+# async def find_all_users():
+#     return user_list_serial(user_col.find())
 
 
 @user_router.get("/get/user/{user_id}", status_code=status.HTTP_200_OK)
@@ -63,53 +55,53 @@ async def find_user_by_id(user_id: str):
         )
 
 
-@user_router.put("/update/user/{user_id}", status_code=status.HTTP_202_ACCEPTED)
-async def update_user(user_id: str, user: User):
-    # Check if the user exists
-    existing_user = user_col.users.find_one({"_id": ObjectId(user_id)})
-    if not existing_user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User with id {user_id} does not exist",
-        )
-    hashed_password = bcrypt_context.hash(user.password)
-    user = User(
-        username=user.username,
-        email=user.email,
-        password=hashed_password,
-        project=existing_user["projec"],
-        logs=existing_user["logs"],
-        created_at=existing_user["created_at"],
-        role=user.role,
-        updated_at=datetime.now(),
-    )
-    # Update the user in the database
-    user_col.users.find_one_and_update(
-        {"_id": ObjectId(user_id)},  # Query by ObjectId
-        {"$set": dict(user)},  # Update with the fields in the User object
-    )
-    updated_user = user_col.users.find_one({"_id": ObjectId(user_id)})
-    if updated_user:
-        return user_individual_serial(updated_user)
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Failed to update user"
-        )
+# @user_router.put("/update/user/{user_id}", status_code=status.HTTP_202_ACCEPTED)
+# async def update_user(user_id: str, user: User):
+#     # Check if the user exists
+#     existing_user = user_col.users.find_one({"_id": ObjectId(user_id)})
+#     if not existing_user:
+#         raise HTTPException(
+#             status_code=status.HTTP_404_NOT_FOUND,
+#             detail=f"User with id {user_id} does not exist",
+#         )
+#     hashed_password = bcrypt_context.hash(user.password)
+#     user = User(
+#         username=user.username,
+#         email=user.email,
+#         password=hashed_password,
+#         project=existing_user["projec"],
+#         logs=existing_user["logs"],
+#         created_at=existing_user["created_at"],
+#         role=user.role,
+#         updated_at=datetime.now(),
+#     )
+#     # Update the user in the database
+#     user_col.users.find_one_and_update(
+#         {"_id": ObjectId(user_id)},  # Query by ObjectId
+#         {"$set": dict(user)},  # Update with the fields in the User object
+#     )
+#     updated_user = user_col.users.find_one({"_id": ObjectId(user_id)})
+#     if updated_user:
+#         return user_individual_serial(updated_user)
+#     else:
+#         raise HTTPException(
+#             status_code=status.HTTP_400_BAD_REQUEST, detail="Failed to update user"
+#         )
 
 
-@user_router.delete("/delete/user/{user_id}", status_code=status.HTTP_202_ACCEPTED)
-async def delete_user(user_id: str):
-    influx = influx_connection()
-    user = user_col.find_one({"_id": ObjectId(user_id)})
-    if user is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found!"
-        )
-    influx.drop_database(user_id)
-    await delete_projects_array(user["project"])
-    logs_col.delete_many({"_id": {"$in": user["logs"]}})
-    user_col.delete_one({"_id": ObjectId(user_id)})
-    return {"message": f"User with id: {user_id} is successfully deleted!"}
+# @user_router.delete("/delete/user/{user_id}", status_code=status.HTTP_202_ACCEPTED)
+# async def delete_user(user_id: str):
+#     influx = influx_connection()
+#     user = user_col.find_one({"_id": ObjectId(user_id)})
+#     if user is None:
+#         raise HTTPException(
+#             status_code=status.HTTP_404_NOT_FOUND, detail="User not found!"
+#         )
+#     influx.drop_database(user_id)
+#     await delete_projects_array(user["project"])
+#     logs_col.delete_many({"_id": {"$in": user["logs"]}})
+#     user_col.delete_one({"_id": ObjectId(user_id)})
+#     return {"message": f"User with id: {user_id} is successfully deleted!"}
 
 
 @user_router.get("/{id}/getall/logs", status_code=status.HTTP_200_OK)
